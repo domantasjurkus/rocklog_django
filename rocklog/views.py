5e0328a1a6f6111292228eb62acc396b9f327737
Domantas Jurkus <domantas.jurkus@skyscanner.net>
2020-04-03 22:16:04 +0100

    simplify comments iin views

--- rocklog/views.py
+++ rocklog/views.py
@@ -74,45 +74,13 @@ def hamster_logout(request):
 def videoid(request, artist, song):
     return HttpResponse(getYoutubeId(artist, song))
 
-    # def detail(request, question_id):
-    #     # try:
-    #     #     question = Question.objects.get(pk=question_id)
-    #     # except Question.DoesNotExist:
-    #     #     raise Http404("Question does not exist")
-    #     question = get_object_or_404(Question, pk=question_id)
-    #     return render(request, 'polls/detail.html', {'question': question})
+    # raise http404 error
+    # question = get_object_or_404(Question, pk=question_id)
 
-    #         """
-    #         Excludes any questions that aren't published yet.
-    #         """
-    #         return Question.objects.filter(date__lte=timezone.now())
+    # # question.choice_set gets auto-generated since Choice has a foreign key to Question
+    # selected_choice = question.choice_set.get(pk=request.POST['choice'])
 
-    # def results(request, question_id):
-    #     question = get_object_or_404(Question, pk=question_id)
-    #     return render(request, 'polls/results.html', {'question': question})
-
-    # class ResultsView(generic.DetailView):
-    #     model = Question
-    #     template_name = 'polls/results.html'
-
-    # def vote(request, question_id):
-    #     question = get_object_or_404(Question, pk=question_id)
-    #     # what if the selected choice does not exist?
-    #     try:
-    #         # question.choice_set gets auto-generated since Choice has a foreign key to Question
-    #         selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    #     except (KeyError, Choice.DoesNotExist):
-    #         # Redisplay the question voting form.
-    #         return render(request, 'polls/detail.html', {
-    #             'question': question,
-    #             'error_message': "You didn't select a choice.",
-    #         })
-    #     else:
-    #         selected_choice.votes += 1
-    #         selected_choice.save()
-    #         # Always return an HttpResponseRedirect after successfully dealing
-    #         # with POST data. This prevents data from being posted twice if a
-    #         # user hits the Back button.
-
-    #         # why do we reverse?
-    #         return HttpResponseRedirect(reverse('pollsapp:results', args=(question.id,)))
+    # # Always return an HttpResponseRedirect after successfully dealing
+    # # with POST data. This prevents data from being posted twice if a
+    # # user hits the Back button.
+    # return HttpResponseRedirect(reverse('pollsapp:results', args=(question.id,)))
