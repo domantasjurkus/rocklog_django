3123a820529c6c4e7763586049177e4566f277fe
Domantas Jurkus <domantas.jurkus@skyscanner.net>
2020-04-03 22:16:59 +0100

    remove comments in urls

--- rocklog/urls.py
+++ rocklog/urls.py
@@ -7,12 +7,9 @@ app_name = 'rocklog'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    # path('index/', views.index, name='index'),
-    # path('home', views.index, name='home'),
-    # path('logout', views.logout),
     path('saved', views.saved_songs, name='saved'),
     path('videoid/<str:artist>/<str:song>/', views.videoid, name='videoid'),
     path('toggle_save_song/<int:song_id>', views.toggle_save_song, name='save_song'),
-    # path('logout/', views.hamster_logout, name='hamster_logout'),
+
     path('logout', LogoutView.as_view(), name='hamster_logout'),
 ]
