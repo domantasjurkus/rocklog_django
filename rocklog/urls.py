219f99eedd556bc0f0b95b61ef616330855daf6b
Domantas Jurkus <domantas.jurkus@skyscanner.net>
2020-04-04 19:56:31 +0100

    rearrange urls

--- rocklog/urls.py
+++ rocklog/urls.py
@@ -7,11 +7,9 @@ app_name = 'rocklog'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('upload/<str:payload>', views.upload_new_song),
-    path('videoid/<str:artist>/<str:song>/', views.videoid, name='videoid'),
-
     path('saved', views.saved_songs, name='saved'),
+    path('upload/<str:payload>', views.upload_new_song),
     path('toggle_save/<int:song_id>', views.toggle_save, name='save_song'),
-
+    path('videoid/<str:artist>/<str:song>/', views.videoid, name='videoid'),
     path('logout', LogoutView.as_view(), name='logout'),
 ]
